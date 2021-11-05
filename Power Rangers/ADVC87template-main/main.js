aa304517994f0700bb22081e35c32c3fa22b35e5
vedraikar <86186191+vedraikar@users.noreply.github.com>
2021-11-05 21:05:48 +0530

    Add files via upload

---
+++ Power Rangers/ADVC87template-main/main.js
@@ -0,0 +1,56 @@
+var canvas = new fabric.canvas("myCanvas");
+
+block_y = 1;
+block_x = 1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object = "";
+
+function new_image(get_image) {
+	fabric.Image.fromURL(get_image, function (Img) {
+		block_image_object = Img;
+		block_image_object.scaletoWidth(block_image_width);
+		block_image_object.scaletoHeight(block_image_height);
+		block_image_object.set({
+			top: block_y,
+			left: block_x
+		})
+		canvas.add(block_image_object);
+	});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if (keyPressed == '82') {
+		new_image = ("rr1.png");
+		console.log("r Key Pressed");
+	}
+	if (keyPressed == '71') {
+		block_x = 200;
+		new_image = ("gr.png");
+		console.log("g Key Pressed");
+	}
+
+	if (keyPressed == '89') {
+		block_x = 350;
+		new_image = ("rr1.png");
+		console.log("r Key Pressed");
+	}
+	if (keyPressed == '80') {
+		block_x = 600;
+		new_image = ("pr.png");
+		console.log("p Key Pressed");
+	}
+	if (keyPressed == '66') {
+		block_x = 700;
+		new_image = ("br.png");
+		console.log("b Key Pressed");
+	}
+
+}
